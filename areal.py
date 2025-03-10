c306f5bd8b34fe380a1c74392f12ae0e93e409a4
iurii Chmykhun <Iurii Chmykhun>
2025-03-10 14:42:38 -0500

    very verified

--- areal.py
+++ areal.py
@@ -1,5 +1,6 @@
 # https://open.kattis.com/problems/areal
 
-# this is also a test of verified commits.
+# this is also a test of verified commits!
+# real test for real this time.
 
 print(int(input()) ** 0.5 * 4)
